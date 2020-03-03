930fab8bab3523317e5ad7603693708ea23e23cf
Remon Karraa <Remon.Karraa@disney.com>
2020-03-03 10:47:18 -0800

    tweaks and cleanup on hadoop-build.sh

--- scripts/hadoop-build.sh
+++ scripts/hadoop-build.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 HADOOP_VERSION=${HADOOP_VERSION:-2.8.5}
 HADOOP_INSTALL_DIR=${HADOOP_INSTALL_DIR:-$HOME/bin/hadoop-${HADOOP_VERSION}}
 
 TMP_BUILD_DIR=/tmp/hadoop-${HADOOP_VERSION}
+PROFILES_DIR="${HOME}"/etc/profile.d/
 
 install_protobuf() {
     PROTO_VERSION=2.5.0
 
     PROTO_TGZ="protobuf-${PROTO_VERSION}.tar.gz"
     echo "Downloading ${PROTO_TGZ} ..."
-    curl --location --silent \
+    curl --silent \
+         --location \
          "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTO_VERSION}/${PROTO_TGZ}" \
        | tar -xf -
 
@@ -25,23 +27,51 @@ install_protobuf() {
     rm -rf protobuf-${PROTO_VERSION}
 }
 
+write_java_profile() {
+  echo "Setting JAVA environment variables..."
+  cat << EOF > "${PROFILES_DIR}"/java.sh
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/
+export PATH=\${PATH}:\${JAVA_HOME}/bin
+EOF
+
+  cat << EOF >> "${HOME}"/.bash_profile
+source "${PROFILES_DIR}"/java.sh
+EOF
+}
+
+install_or_check_java() {
+
+  if ! brew cask list adoptopenjdk8; then
+      echo "Install OpenJDK8..."
+      brew tap AdoptOpenJDK/openjdk
+      brew cask install adoptopenjdk8
+      write_java_profile
+  fi
+
+  if [ -z "${JAVA_HOME}" ]; then
+    if [ -f "${PROFILES_DIR}"/java.sh ]; then
+      # shellcheck source=/dev/null
+      source "${PROFILES_DIR}"/java.sh
+    fi
+  fi
+
+  if [ -z "${JAVA_HOME}" ]; then
+    echo "JAVA_HOME is not set, exiting"
+    exit 1
+  fi
+}
+
 install_prerequisites() {
     echo "Installing prerequisites ..."
-    if [ -z "$(which brew)" ]; then
+    if [ -z "$(brew --version)" ]; then
         echo "Installing brew..."
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"   
     fi
 
-    brew update || brew update
+    brew update
     brew upgrade
-    brew cask list|grep adoptopenjdk8
-    if [ $? -ne 0 ]; then
-        echo "Install OpenJDK8..."
-        brew tap AdoptOpenJDK/openjdk
-        brew cask install adoptopenjdk8
-    fi
-
-    brew install r sbt maven gcc autoconf automake libtool cmake snappy gzip bzip2 zlib openssl
+    install_or_check_java
+    brew install -f r sbt maven gcc autoconf automake libtool cmake snappy gzip bzip2 zlib openssl
     ln -f -s /usr/local/include/opt/openssl/include/openssl /usr/local/include/openssl
     install_protobuf
 }
@@ -102,17 +132,23 @@ install_hadoop() {
     cp -R "hadoop-dist/target/hadoop-${HADOOP_VERSION}" "${HADOOP_INSTALL_DIR}"
 }
 
-setup_env_vars() {
-    echo "Setting HADOOP environment variables in ${HOME}/.bash_profile..."
-    cat << EOF >> "${HOME}"/.bash_profile
+write_hadoop_profile() {
+    echo "Setting hadoop environment variables..."
+    cat << EOF > "${PROFILES_DIR}"/hadoop.sh
 export HADOOP_VERSION=${HADOOP_VERSION}
-export HADOOP_HOME="${HADOOP_INSTALL_DIR}
+export HADOOP_HOME=${HADOOP_INSTALL_DIR}
 export HADOOP_OPTS="-Djava.library.path=\${HADOOP_HOME}/lib/native"
 export LD_LIBRARY_PATH=\${HADOOP_HOME}/lib/native\${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}
 export JAVA_LIBRARY_PATH=\${JAVA_LIBRARY_PATH}:\${HADOOP_HOME}/lib/native
 export PATH=\${PATH}:\${HADOOP_HOME}/bin
 EOF
 
+  cat << EOF >> "${HOME}"/.bash_profile
+source ${PROFILES_DIR}/hadoop.sh
+EOF
+
+  # shellcheck source=/dev/null
+  source "${PROFILES_DIR}"/hadoop.sh
 }
 
 cleanup() {
@@ -122,12 +158,12 @@ cleanup() {
 }
 
 main() {
+    mkdir -p "${PROFILES_DIR}"
     mkdir -p "${TMP_BUILD_DIR}"
     cd "${TMP_BUILD_DIR}"
     install_prerequisites
     install_hadoop
-    setup_env_vars
-    source "${HOME}"/.bash_profile
+    write_hadoop_profile
     hadoop checknative -a
     cleanup
     echo "Done. Happy Hadooping!!!"
